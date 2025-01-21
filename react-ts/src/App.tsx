71d9c987aaed600867ead8e2eff1bbe3120bf85b
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-21 22:05:29 +0545

    add routing for Malay, Arab, and All components in App

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/Home";
+import Malay from "./pages/Malay";
+import Arab from "./pages/Arab";
+import All from "./pages/All";
 
 const App = () => {
   return (
@@ -7,6 +10,9 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/malay" element={<Malay />} />
+          <Route path="/arab" element={<Arab />} />
+          <Route path="/all" element={<All />} />
         </Routes>
       </BrowserRouter>
     </div>
