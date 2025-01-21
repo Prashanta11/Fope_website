c37dc6611b90388a36b066c998b34761d830f0a7
Prashanta Deuja <prashantadeuja@gmail.com>
2025-01-21 14:58:32 +0545

    add background image and integrate HeroSection into Home component

--- react-ts/src/pages/Home.tsx
+++ react-ts/src/pages/Home.tsx
@@ -1,9 +1,23 @@
+import backgroundImage from "../assets/images/background.webp";
+import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <div
+        className="relative flex justify-center items-center bg-cover bg-no-repeat bg-center min-h-screen text-black"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+        }}
+      >
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-black opacity-0"></div>
+
+        {/* Hero Section Content */}
+        <HeroSection />
+      </div>
     </div>
   );
 };
